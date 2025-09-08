5d3eb420b896b060be26808ca6e64472b0f7329e
Ankitatiwary21 <125357910+Ankitatiwary21@users.noreply.github.com>
2025-09-09 01:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (58.05%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+         int a = n;
+        int b = 0;
+        int placeValue = 1;
+
+        while (n > 1) {
+            int adjust = 1;
+
+            // If the last digit is 1, subtracting 1 would make 'a' have a zero → avoid
+            if (n % 10 == 1) {
+                adjust = 2;
+            }
+
+            a -= adjust * placeValue;
+            b += adjust * placeValue;
+
+            n = (n - adjust) / 10; // move to next digit
+            placeValue *= 10;
+        }
+
+        return new int[]{a, b};
+    }
+}
