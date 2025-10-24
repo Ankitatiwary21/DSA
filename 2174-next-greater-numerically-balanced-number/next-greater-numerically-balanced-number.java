c40fbeadbfb5b5bfdb96a7c7f5f750fa4df3a6eb
Ankitatiwary21 <125357910+Ankitatiwary21@users.noreply.github.com>
2025-10-24 23:54:45 +0530

    Time: 92 ms (38.46%) | Memory: 44.5 MB (60.00%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        int num = n + 1;
+        while (true) {
+            if (isBalanced(num)) return num;
+            num++;
+        }
+    }
+    
+    private boolean isBalanced(int num) {
+        int[] count = new int[10];
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            count[digit]++;
+            temp /= 10;
+        }
+        for (int d = 0; d < 10; d++) {
+            if (count[d] != 0 && count[d] != d) return false;
+        }
+        return true;
+    }
+}
